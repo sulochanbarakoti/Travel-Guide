e54ef9f7b294ef77a5afb783087987d9654b4dd2
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-03-01 13:55:07 +0200

    update image in story section

--- travel-guide/src/components/Stories.js
+++ travel-guide/src/components/Stories.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Button, Col, Container, Row, Image } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Row,
+  Image,
+  CardGroup,
+  Card,
+} from "react-bootstrap";
 // import { elephantSafari } from "../asset/logic";
 import { chitwan, kathmandu, pokhara, everest } from "../asset/logic";
 
@@ -7,6 +15,15 @@ import { stories } from "../data";
 
 const Stories = () => {
   console.log(stories);
+  // Function to generate a random number between min (inclusive) and max (exclusive)
+  const getRandomNumber = (min, max) =>
+    Math.floor(Math.random() * (max - min) + min);
+
+  // Selecting four random objects from the array
+  const randomObjects = Array.from(
+    { length: 4 },
+    () => stories[getRandomNumber(0, stories.length)]
+  );
   return (
     <Container>
       <Row className="fw-bold">
@@ -21,15 +38,37 @@ const Stories = () => {
           <Button variant="outline-success">Read more articles</Button>
         </Col>
       </Row>
-      <Row>
+      <Row className="mt-3">
         <Col>
-          <Image className="story-thumb-img" src={kathmandu} thumbnail />
-          <div className="story-thumb-text">
-            <div></div>
-            <div>18 of the best free things to do in Sauraha</div>
-          </div>
+          <CardGroup>
+            <Card className="bg-dark text-white">
+              <Card.Img src={chitwan} alt="Chitwan National Park" />
+              <Card.ImgOverlay>
+                <Card.Title>Chitwan National Park</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+          </CardGroup>
         </Col>
-        <Col></Col>
+        <Col>
+          <Row xs={1} md={2} className="g-2">
+            {randomObjects.map((item) => (
+              <Col key={item.id}>
+                <Card className="bg-dark text-white">
+                  <Card.Img src={item.image} alt="Chitwan National Park" />
+                  <Card.ImgOverlay>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.description}</Card.Text>
+                  </Card.ImgOverlay>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
       </Row>
     </Container>
   );
--- travel-guide/src/data.js
+++ travel-guide/src/data.js
@@ -22,37 +22,41 @@ export const searchData = ["trek", "raffle", "paraglide"];
 
 export const stories = [
   {
+    id: 1,
     title: "Exploring the Cultural Treasures of Kathmandu Valley",
     location: "Kathmandu",
     description:
       "The Kathmandu Valley is a cultural hub and home to several UNESCO World Heritage Sites, including Kathmandu Durbar Square, Patan Durbar Square, and Bhaktapur Durbar Square. Visitors can explore ancient temples, palaces, and bustling marketplaces, immersing themselves in the rich history and architecture of the region.",
-    image: { kathmandu },
+    image: kathmandu,
     date: "2023-08-15",
   },
   {
+    id: 2,
     title:
       "Journey to the Roof of the World: Trekking to Mount Everest Base Camp",
     location: "Solukhumbu District",
     description:
       "Mount Everest, the highest peak in the world, attracts adventurers and climbers from around the globe. While summiting Everest is a challenging feat reserved for experienced mountaineers, trekking to Everest Base Camp offers a memorable journey through the stunning Himalayan landscape.",
-    image: { everest },
+    image: everest,
     date: "2023-09-20",
   },
   {
+    id: 3,
     title: "Discovering Paradise: Exploring Pokhara's Natural Wonders",
     location: "Gandaki Province",
     description:
       "Pokhara is known for its breathtaking natural beauty, with stunning views of the Annapurna and Dhaulagiri mountain ranges reflected in the serene waters of Phewa Lake. Visitors can enjoy boating on the lake, exploring caves, and hiking to nearby viewpoints for panoramic vistas.",
-    image: { pokhara },
+    image: pokhara,
     date: "2023-10-10",
   },
   {
+    id: 4,
     title:
       "Wildlife Safari in Chitwan National Park: A Journey into the Heart of Nature",
     location: "Chitwan District",
     description:
       "Chitwan National Park is a haven for wildlife enthusiasts, home to a diverse range of flora and fauna, including endangered species such as the Bengal tiger and one-horned rhinoceros. Visitors can enjoy jungle safaris, birdwatching, and cultural experiences with the indigenous Tharu community.",
-    image: { chitwan },
+    image: chitwan,
     date: "2023-11-05",
   },
 ];
